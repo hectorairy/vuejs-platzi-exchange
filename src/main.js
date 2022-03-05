eb3e4c6c05a98dde01ead8250b318637d129a9c7
Héctor Airy <hola@hectorairy.com>
2022-03-04 19:41:49 -0600

    finish platzi exchange project from vuejs2 basic course

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "@/assets/css/tailwind.css";
+import routes from "@/routes";
+import { dollarFilter, percentFilter } from "@/filters";
 
+Vue.filter("dollar", dollarFilter);
+Vue.filter("percent", percentFilter);
 Vue.config.productionTip = false;
 
 new Vue({
+  router: routes,
+
   render: (h) => h(App),
 }).$mount("#app");
